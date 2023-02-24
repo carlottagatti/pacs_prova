a8c78fb2edefbb4e34945708423ab54fe73ae346
Carlotta Gatti <carlottagatti@live.it>
2023-02-24 14:53:40 +0100

    modified the 2 files

---
+++ file1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Ciao, sono il file 1" << std::endl;
+
+    return 0;
+}
---
+++ file2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Ciao, sono il file 2" << std::endl;
+
+    return 0;
+}
