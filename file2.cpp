bb4b57555a12fce9e6af7bdb24aef110f6d2fe1a
Carlotta Gatti <carlottagatti@live.it>
2023-02-24 14:56:45 +0100

    file 2 modified in branch1

--- file2.cpp
+++ file2.cpp
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    std::cout << "Ciao, sono il file 2" << std::endl;
+    std::cout << "Ciao, sono il file 2 e sono stato modificato in branch1" << std::endl;
 
     return 0;
 }
